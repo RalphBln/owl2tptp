d2a59e6475412d13614ba6f4b847fde9f99c5dc2
Ralph Schaefermeier <ralph.schafermeier@gmail.com>
2021-02-28 16:21:21 +0100

    Switch to Junit 5 and use ParameterizedTest

--- src/test/java/xyz/aspectowl/reasoner/test/OWL2ReasonerTest.java
+++ src/test/java/xyz/aspectowl/reasoner/test/OWL2ReasonerTest.java
@@ -1,16 +1,20 @@
 package xyz.aspectowl.reasoner.test;
 
+import net.sf.tweety.logics.fol.parser.FolParser;
 import net.sf.tweety.logics.fol.reasoner.FolReasoner;
+import net.sf.tweety.logics.fol.syntax.FolBeliefSet;
 import net.sf.tweety.logics.fol.syntax.FolFormula;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.io.OWLRendererException;
-import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 import xyz.aspectowl.tptp.reasoner.SpassTptpFolReasoner;
 import xyz.aspectowl.tptp.renderer.OWL2TPTPObjectRenderer;
-import xyz.aspectowl.tptp.renderer.OWL2TPTPRenderer;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,6 +23,9 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author ralph
@@ -27,43 +34,50 @@ public class OWL2ReasonerTest {
 
     private static final IRI conjectureProp = IRI.create("http://ontology.aspectowl.xyz/reasoner/aspectowl2tptp/test/base#conjecture");
 
-    private OWLOntologyManager man;
-    private FolReasoner prover;
+    private static FolReasoner prover;
 
-    private String spassLocation;
-
-    @Before
-    public void setup() {
-        spassLocation = Optional.of(System.getenv("SPASS_HOME")).orElseThrow(() -> new IllegalStateException("SPASS_HOME environment variable must point to SPASS installation directory"));
-
-        man = OWLManager.createOWLOntologyManager();
-        man.getIRIMappers().add(ontologyIRI -> IRI.create(OWL2ReasonerTest.class.getResource("/ontologies/test-base.ofn")));
-
+    @BeforeAll
+    public static void setup() {
+        String spassLocation = Optional.of(System.getenv("SPASS_HOME")).orElseThrow(() -> new IllegalStateException("SPASS_HOME environment variable must point to SPASS installation directory"));
         FolReasoner.setDefaultReasoner(new SpassTptpFolReasoner(spassLocation)); //Set default prover, options are NaiveProver, EProver, Prover9
         prover = FolReasoner.getDefaultReasoner();
     }
 
-    @Test
-    public void reasonOnOntologies() throws URISyntaxException {
+    @ParameterizedTest
+    @MethodSource("provideOntologySourceLocations")
+    public void reasonOnOntologies(FolBeliefSet bs, FolFormula conjecture)  {
+        assertTrue(prover.query(bs, conjecture));
+    }
+
+    private static Stream<Arguments> provideOntologySourceLocations() throws URISyntaxException {
+
+        OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+        man.getIRIMappers().add(ontologyIRI -> IRI.create(OWL2ReasonerTest.class.getResource("/ontologies/test-base.ofn")));
+
         URL ontologiesBaseURL = OWL2ReasonerTest.class.getResource("/ontologies");
         File ontologiesBaseDir = new File(ontologiesBaseURL.toURI());
-        Arrays.stream(ontologiesBaseDir.listFiles(file -> !file.getName().equals("test-base.ofn") && file.getName().endsWith(".ofn"))).forEach(file -> {
+        return Arrays.stream(ontologiesBaseDir.listFiles(file -> !file.getName().equals("test-base.ofn") && file.getName().endsWith(".ofn"))).flatMap(file -> {
             try {
                 OWLOntology onto = man.loadOntologyFromOntologyDocument(OWL2ReasonerTest.class.getResourceAsStream("/ontologies/" + file.getName()));
                 OWL2TPTPObjectRenderer renderer = new OWL2TPTPObjectRenderer(onto, new PrintWriter(System.out));
                 onto.accept(renderer);
-                onto.annotations(man.getOWLDataFactory().getOWLAnnotationProperty(conjectureProp)).forEach(annotation ->
-                        annotation.getValue().asLiteral().ifPresent(literal -> {
-                            try {
-                                assertTrue(prover.query(renderer.getBeliefSet(), (FolFormula) renderer.getFolParser().parseFormula(literal.getLiteral())));
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }));
+                return onto.annotations(man.getOWLDataFactory().getOWLAnnotationProperty(conjectureProp)).map(annotation ->
+                        annotation.getValue().asLiteral().map(literal ->
+                                Arguments.of(renderer.getBeliefSet(), parseFormula(literal.getLiteral(), renderer.getFolParser()).orElseThrow(() -> new RuntimeException()))
+                        ).get());
             } catch (OWLOntologyCreationException e) {
                 throw new RuntimeException(e);
             }
         });
+    }
 
+    private static Optional<FolFormula> parseFormula(String formula, FolParser parser) {
+        FolFormula result = null;
+        try {
+            result = parser.parseFormula(formula);
+        } catch (IOException e) {
+            return Optional.empty();
+        }
+        return Optional.of(result);
     }
 }
